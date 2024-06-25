79b67eebabd8ffc32107cd568697e457b38d9052
Sanket Khule <sanket.khule@equip9.com>
2024-06-25 20:31:39 +0530

    capstone update

--- index.js
+++ index.js
@@ -52,9 +52,11 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+
+
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 /* MONGOOSE SETUP */
